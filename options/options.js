41efc25e85375ec453aa0a48d1b34d72b842035e
Crowdbotics <team@crowdbotics.com>
2023-09-04 13:35:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "betting-tips-43223",
+  url: "https://betting-tips-43223.botics.co",
+  api: "https://betting-tips-43223.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
